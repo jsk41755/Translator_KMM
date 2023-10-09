5b35fce3634e8ea948a9f92f4bd68571a31694fa
JeongSG <jsk41755@naver.com>
2023-10-10 01:47:56 +0900

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+buildscript {
+    val agp_version by extra("7.4.2")
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath(Deps.kotlinGradlePlugin)
+        classpath(Deps.androidBuildTools)
+        classpath(Deps.sqlDelightGradlePlugin)
+        classpath(Deps.hiltGradlePlugin)
+        classpath("com.android.tools.build:gradle:$agp_version")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
