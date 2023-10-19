c993b0e424408f4809e9d9d1a31085239cdaca14
JeongSG <jsk41755@naver.com>
2023-10-19 22:32:30 +0900

    2.4 Translate Use Case

---
+++ shared/src/commonMain/kotlin/com/devjeong/translator_kmm/translate/domain/translate/Translate.kt
@@ -0,0 +1,38 @@
+package com.devjeong.translator_kmm.translate.domain.translate
+
+import com.devjeong.translator_kmm.core.domain.language.Language
+import com.devjeong.translator_kmm.core.domain.util.Resource
+import com.devjeong.translator_kmm.translate.domain.history.HistoryDataSource
+import com.devjeong.translator_kmm.translate.domain.history.HistoryItem
+
+class Translate(
+    private val client: TranslateClient,
+    private val historyDataSource: HistoryDataSource
+) {
+    suspend fun execute(
+        fromLanguage: Language,
+        fromText: String,
+        toLanguage: Language
+    ): Resource<String>{
+        return try {
+            val translatedText = client.translate(
+                fromLanguage, fromText, toLanguage
+            )
+
+            historyDataSource.insertHistoryItem(
+                HistoryItem(
+                    id = null,
+                    fromLanguageCode = fromLanguage.langCode,
+                    fromText = fromText,
+                    toLanguageCode = toLanguage.langCode,
+                    toText = translatedText,
+                )
+            )
+
+            Resource.Success(translatedText)
+        } catch (e: TranslateException){
+            e.printStackTrace()
+            Resource.Error(e)
+        }
+    }
+}
