7ee7b2b1a8fe1b007c1ffa64190e9cd5baac1ff8
JeongSG <jsk41755@naver.com>
2023-10-25 16:11:41 +0900

    3.2 Building the Language Pickers

---
+++ androidApp/src/main/java/com/devjeong/translator_kmm/android/TranslateApp.kt
@@ -0,0 +1,7 @@
+package com.devjeong.translator_kmm.android
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class TranslateApp: Application()
---
+++ androidApp/src/main/java/com/devjeong/translator_kmm/android/core/presentation/Routes.kt
@@ -0,0 +1,6 @@
+package com.devjeong.translator_kmm.android.core.presentation
+
+object Routes {
+    const val TRANSLATE = "translate"
+    const val VOICE_TO_TEXT = "voice_to_text"
+}
