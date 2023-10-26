e922326690a46c0991d484b1c905160145e69313
JeongSG <jsk41755@naver.com>
2023-10-26 13:48:18 +0900

    3.3 Building the Translation Text Field

--- androidApp/src/main/java/com/devjeong/translator_kmm/android/translate/presentation/TranslateScreen.kt
+++ androidApp/src/main/java/com/devjeong/translator_kmm/android/translate/presentation/TranslateScreen.kt
@@ -1,5 +1,8 @@
+@file:OptIn(ExperimentalComposeUiApi::class)
+
 package com.devjeong.translator_kmm.android.translate.presentation
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -10,10 +13,17 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalClipboardManager
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.unit.dp
+import com.devjeong.translator_kmm.android.R
 import com.devjeong.translator_kmm.android.translate.presentation.components.LanguageDropDown
 import com.devjeong.translator_kmm.android.translate.presentation.components.SwapLanguagesButton
+import com.devjeong.translator_kmm.android.translate.presentation.components.TranslateTextField
 import com.devjeong.translator_kmm.translate.presentation.TranslateEvent
 import com.devjeong.translator_kmm.translate.presentation.TranslateState
 
@@ -22,6 +32,7 @@ fun TranslateScreen(
     state: TranslateState,
     onEvent: (TranslateEvent) -> Unit
 ) {
+    val context = LocalContext.current
     Scaffold(
         floatingActionButton = {
 
@@ -73,6 +84,47 @@ fun TranslateScreen(
                         }
                     )
                 }
+            }
+            item {
+                val clipboardManager = LocalClipboardManager.current
+                val keyboardController = LocalSoftwareKeyboardController.current
+                TranslateTextField(
+                    fromText = state.fromText,
+                    toText = state.toText,
+                    isTranslating = state.isTranslating,
+                    fromLanguage = state.fromLanguage,
+                    toLanguage = state.toLanguage,
+                    onTranslateClick = {
+                        keyboardController?.hide()
+                        onEvent(TranslateEvent.Translate)
+                    },
+                    onTextChange = {
+                        onEvent(TranslateEvent.ChangeTranslationText(it))
+                    },
+                    onCopyClick = {text ->
+                        clipboardManager.setText(
+                            buildAnnotatedString {
+                                append(text)
+                            }
+                        )
+                        Toast.makeText(
+                            context,
+                            context.getString(R.string.copied_to_clipboard
+                            ),
+                            Toast.LENGTH_LONG
+                        ).show()
+                    },
+                    onCloseClick = {
+                        onEvent(TranslateEvent.CloseTranslation)
+                    },
+                    onSpeakerClick = {
+
+                                     },
+                    onTextFieldClick = {
+                        onEvent(TranslateEvent.EditTranslation)
+                    },
+                    modifier = Modifier.fillMaxWidth()
+                )
             }
         }
     }
