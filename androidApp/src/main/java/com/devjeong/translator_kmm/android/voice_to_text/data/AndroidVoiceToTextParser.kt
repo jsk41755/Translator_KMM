a385159084609cb4f2e50bb1f5375c0115bcba31
JeongSG <jsk41755@naver.com>
2023-11-06 13:57:05 +0900

    5.1 Speech Recognition on Android

---
+++ androidApp/src/main/java/com/devjeong/translator_kmm/android/voice_to_text/data/AndroidVoiceToTextParser.kt
@@ -0,0 +1,99 @@
+package com.devjeong.translator_kmm.android.voice_to_text.data
+
+import android.app.Application
+import android.content.Intent
+import android.os.Bundle
+import android.speech.RecognitionListener
+import android.speech.RecognizerIntent
+import android.speech.SpeechRecognizer
+import com.devjeong.translator_kmm.android.R
+import com.devjeong.translator_kmm.core.domain.util.CommonMutableStateFlow
+import com.devjeong.translator_kmm.core.domain.util.CommonStateFlow
+import com.devjeong.translator_kmm.core.domain.util.toCommonStateFlow
+import com.devjeong.translator_kmm.voice_to_text.domain.VoiceToTextParser
+import com.devjeong.translator_kmm.voice_to_text.domain.VoiceToTextParserState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class AndroidVoiceToTextParser(
+    private val app: Application
+): VoiceToTextParser, RecognitionListener {
+
+    private val recognizer = SpeechRecognizer.createSpeechRecognizer(app)
+
+    private val _state = MutableStateFlow(VoiceToTextParserState())
+    override val state: CommonStateFlow<VoiceToTextParserState> =
+        _state.toCommonStateFlow()
+
+    override fun startListening(languageCode: String) {
+        _state.update { VoiceToTextParserState() }
+
+        if(!SpeechRecognizer.isRecognitionAvailable(app)){
+            _state.update { it.copy(
+                error = app.getString(R.string.error_speech_recognition_unavailable)
+            ) }
+            return
+        }
+
+        val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH).apply {
+            putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+            putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, languageCode)
+        }
+        recognizer.setRecognitionListener(this)
+        recognizer.startListening(intent)
+        _state.update { it.copy(
+            isSpeaking = true
+        ) }
+    }
+
+    override fun stopListening() {
+        _state.update { VoiceToTextParserState() }
+        recognizer.stopListening()
+    }
+
+    override fun cancel() {
+        recognizer.cancel()
+    }
+
+    override fun reset() {
+        _state.value = VoiceToTextParserState()
+    }
+
+    override fun onReadyForSpeech(params: Bundle?) {
+        _state.update { it.copy(error = null) }
+    }
+
+    override fun onBeginningOfSpeech() = Unit
+
+    override fun onRmsChanged(rmsdB: Float) {
+        _state.update { it.copy(
+            powerRatio = rmsdB * (1f / (12f - (-2f)))
+        ) }
+    }
+
+    override fun onBufferReceived(buffer: ByteArray?) = Unit
+
+    override fun onEndOfSpeech() {
+        _state.update { it.copy(isSpeaking = false) }
+    }
+
+    override fun onError(code: Int) {
+        _state.update { it.copy(error = "Error: $code") }
+    }
+
+    override fun onResults(result: Bundle?) {
+        result
+            ?.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)
+            ?.getOrNull(0)
+            ?.let { text ->
+                _state.update { it.copy(
+                    result = text
+                ) }
+            }
+    }
+
+    override fun onPartialResults(partialResults: Bundle?) = Unit
+
+    override fun onEvent(eventType: Int, params: Bundle?) = Unit
+}
---
+++ shared/src/commonMain/kotlin/com/devjeong/translator_kmm/voice_to_text/domain/VoiceToTextParserState.kt
@@ -0,0 +1,9 @@
+package com.devjeong.translator_kmm.voice_to_text.domain
+
+data class VoiceToTextParserState(
+    val result: String = "",
+    val error: String? = null,
+    val powerRatio: Float = 0f,
+    val isSpeaking: Boolean = false
+
+)
